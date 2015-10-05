dfae11572c5d3da1c08928fd969fa6b65f9a2c62
wouter <wouter@lagerweij.com>
2015-10-05 17:14:27 +0200

    Set template for ebook use. Can't get if statements to work

--- script/create_book.sh
+++ script/create_book.sh
@@ -10,5 +10,5 @@ cp /manuscript/*.tex /manuscript/*.yml /target
 ls -1 /manuscript/*.txt | while read line; do cat $line |sed -e "s/^I>/> /" | sed -e "s/^A>/> /" > /target/$(basename $line).converted.md; done
 ls -1 *.md | while read line ; do mv $line $(echo $line | sed -e "s/txt.converted.md/txt/"); done
 
-pandoc --template=${1}.latex -V documentclass=book --toc -o /target/${2}.pdf ${@:3}
+pandoc --template=${1} -V documentclass=book --toc -o /target/${2} ${@:3}
 echo "Done!"
